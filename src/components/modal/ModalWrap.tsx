895bc7897136e93e061250373f11aa3f3a161ed7
ikhyeons <skantrkwl789@naver.com>
2023-12-15 16:46:14 +0900

    Update : 모달 랩 생성

---
+++ src/components/modal/ModalWrap.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const SModalWrap = styled.div`
+  position: fixed;
+  width: 1200dvw;
+  height: 100dvh;
+  rgba(0, 0, 0, 0.1)
+`;
+
+function ModalWrap() {
+  return <SModalWrap></SModalWrap>;
+}
+
+export default ModalWrap;
