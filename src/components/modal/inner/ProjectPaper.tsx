d7bf6046b7ffcc8f8dde55073a320c5a0de88c3c
ikhyeons <skantrkwl789@naver.com>
2023-12-18 04:51:54 +0900

    Update : 프로젝트 모달 초기세팅

--- src/components/modal/inner/ProjectPaper.tsx
+++ src/components/modal/inner/ProjectPaper.tsx
@@ -1,8 +1,32 @@
 import React from "react";
 import projectData from "../../../projectData";
+import styled from "styled-components";
 
+const Swrap = styled.div`
+  width: 40%;
+  margin: 0 auto;
+  background: #fafafa;
+  height: 90%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  padding: 20px;
+  box-sizing: border-box;
+  cursor: auto;
+  border-radius: 5px;
+  box-shadow: 3px 3px 5px black;
+`;
 function ProjectPaper() {
-  return <div></div>;
+  return (
+    <Swrap
+      onClick={(e) => {
+        e.stopPropagation();
+        e.preventDefault();
+      }}
+    >
+      gd
+    </Swrap>
+  );
 }
 
 export default ProjectPaper;
