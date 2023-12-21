d7f04811cdf95678305f6ba1892a87cb11471d72
ikhyeons <skantrkwl789@naver.com>
2023-12-22 00:20:18 +0900

    Update : 데이터 서머리 수정

--- src/moreData.ts
+++ src/moreData.ts
@@ -9,7 +9,7 @@ export const data = [
       deploy: [],
     },
     summary:
-      "아두이노, 라즈베리파이와 openAI에서 제공하는 GPT3.5 Turbo모델을 이용하여 인공지능 비서를 구현하였습니다.",
+      "아두이노, 라즈베리파이와 GPT3.5 Turbo모델을 이용하여 인공지능 비서를 구현하였습니다.",
   },
   {
     title: "텔로 드론 프로젝트",
